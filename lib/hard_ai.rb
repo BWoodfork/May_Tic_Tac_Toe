52cc4bcfd2bd7736336e10df217494b7a53520ba
Byron Woodfork <byronwoodfork@gmail.com>
2014-08-04 10:16:42 -0500

    Deleted everything from HardAI class.

--- lib/hard_ai.rb
+++ lib/hard_ai.rb
@@ -1,21 +1,3 @@
 class HardAI
-  attr_reader :token
 
-  def initialize(board)
-    @board = board
-    @token = token
-  end
-
-
-  def get_empty_spaces(board)
-    fake_board = clone_board(board).spaces
-
-    fake_board.each_index.select do |spot|
-      fake_board[spot] == nil
-    end
-  end
-
-  def clone_board(board)
-    Marshal.load(Marshal.dump(board))
-  end
 end
