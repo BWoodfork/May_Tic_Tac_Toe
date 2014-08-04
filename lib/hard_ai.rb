a08f9715552133cd623ef71d3a9cc7be1f92bd97
Byron Woodfork <byronwoodfork@gmail.com>
2014-08-04 10:05:42 -0500

    Deleted MiniMax method. Starting from scratch.

--- lib/hard_ai.rb
+++ lib/hard_ai.rb
@@ -6,62 +6,8 @@ class HardAI
     @token = token
   end
 
-  def make_move(board)
-    fake_board = clone_board(board)
-    move = minimax(fake_board)
 
-    @board.fill_space(move, @board.o_mark)
-  end
-
-  def minimax(fake_board)
-    empty_spots = check_empty_spaces(fake_board)
-    best_score = {}
-
-    empty_spots.each do |space|
-      p space
-      
-      fake_board.fill_space(space, fake_board.token_that_is_up)
-        if fake_board.game_over? && fake_board.winner == fake_board.token_that_is_up
-          best_score[space] = 100
-        elsif fake_board.game_over? && fake_board.winner == fake_board.x_mark
-          best_score[space] = 0
-        elsif fake_board.tie_game? && fake_board.winner.nil? && fake_board.game_over?
-          best_score[space] = 50
-        else
-          minimax(fake_board)
-        end
-    end
-
-        if fake_board.token_that_is_up == fake_board.o_mark
-          number = best_score.values.max
-          best_score.key(number)
-        elsif fake_board.token_that_is_up == fake_board.x_mark
-          number = best_score.values.min
-          best_score.key(number)
-        end
-  end
-
-          # if !fake_board.game_over?
-        #   minimax(board)
-        # elsif fake_board.game_over? && fake_board.winner == fake_board.token_that_is_up
-        #   best_score[space] = 100
-        # elsif fake_board.game_over? && fake_board.tie_game?
-        #   best_score[space] = 50
-        # elsif fake_board.game_over? && fake_board.winner == fake_board.x_mark
-        #   best_score[space] = 0
-        # end
-
-  def score_move(board)
-    if board.winner == board.o_mark
-      100
-    elsif board.winner == board.x_mark
-      0
-    else
-      50
-    end
-  end
-
-  def check_empty_spaces(board)
+  def get_empty_spaces(board)
     fake_board = clone_board(board).spaces
 
     fake_board.each_index.select do |spot|
--- spec/hard_ai_spec.rb
+++ spec/hard_ai_spec.rb
@@ -20,7 +20,7 @@ describe HardAI do
 
     end
 
-    it "should return the move to tie the game" do
+    xit "should return the move to tie the game" do
       @board.fill_space(0, "X")
       @board.fill_space(1, "O")
       @board.fill_space(2, "X")
@@ -35,7 +35,7 @@ describe HardAI do
       @hard.minimax(@board).should == 8
     end
 
-    it "should return the move to block a win" do
+    xit "should return the move to block a win" do
       @board.fill_space(0, "X")
       @board.fill_space(7, "O")
       @board.fill_space(2, "X")
@@ -58,7 +58,7 @@ describe HardAI do
       @board.fill_space(7, "X")
 
 
-      @hard.check_empty_spaces(@board).should == [8]
+      @hard.get_empty_spaces(@board).should == [8]
     end
   end
   
@@ -71,52 +71,6 @@ describe HardAI do
     it "should clone duplicate spaces" do
       new_board = @board.spaces
       @hard.clone_board(@board).object_id.should_not == @board.object_id
-    end
-  end
-
-  context "#score" do
-    describe "scoring conditions" do
-      it "should return score of 100 at a win" do
-        @board.fill_space(0, "O")
-        @board.fill_space(1, "X")
-        @board.fill_space(2, "X")
-
-        @board.fill_space(3, "O")
-        @board.fill_space(4, "X")
-        @board.fill_space(6, "O")
-
-        @hard.score_move(@board).should == 100
-      end
-
-      it "should return score of 0 at a loss" do
-        @board.fill_space(0, "O")
-        @board.fill_space(1, "X")
-        @board.fill_space(2, "X")
-
-        @board.fill_space(4, "O")
-        @board.fill_space(5, "X")
-        @board.fill_space(6, "O")
-
-        @board.fill_space(8, "X")
-
-        @hard.score_move(@board).should == 0
-      end
-
-      it "should return a score of 50 at a tie" do
-        @board.fill_space(0, "O")
-        @board.fill_space(1, "X")
-        @board.fill_space(2, "O")
-
-        @board.fill_space(3, "X")
-        @board.fill_space(4, "X")
-        @board.fill_space(5, "O")
-
-        @board.fill_space(6, "O")
-        @board.fill_space(7, "O")
-        @board.fill_space(8, "X")
-
-        @hard.score_move(@board).should == 50
-      end
     end
   end
 end
